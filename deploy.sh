c5bb1786521cc00cc7fe828c1ab8ebd622396e3c
akapoorcern <akapoor@cern.ch>
2022-04-12 14:39:55 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 cd ..
 pip install mkdocs-material
 rm -rf cms-egamma.github.io
-git clone https://github.com/cms-egamma/cms-egamma.github.io.git
+gh repo clone cms-egamma/cms-egamma.github.io
+#git clone https://github.com/cms-egamma/cms-egamma.github.io.git
 cd cms-egamma.github.io/
 mkdocs gh-deploy --config-file ../cms-egamma-docs/mkdocs.yml --remote-branch master
 cd ./../cms-egamma-docs
