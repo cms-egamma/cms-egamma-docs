7cfba886577607c81020ad64deab84d94e94c7f7
akapoorcern <akapoor@cern.ch>
2022-04-12 14:34:05 +0200

    Testing deploying

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-cd cms-egamma.github.io/;mkdocs gh-deploy --config-file ../mkdocs.yml --remote-branch master;cd ..
+cd ..
+rm -rf cms-egamma.github.io
+git clone https://github.com/cms-egamma/cms-egamma.github.io.git
+cd cms-egamma.github.io/
+mkdocs gh-deploy --config-file ../cms-egamma-docs/mkdocs.yml --remote-branch master
+cd ./../cms-egamma-docs
