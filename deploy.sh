d6789f64324aa3ddacefd82831181406d40467e1
akapoorcern <akapoor@cern.ch>
2022-04-13 00:27:26 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@ cd ../
 pip install mkdocs-material
 rm -rf cms-egamma.github.io
 #git clone https://github.com/cms-egamma/cms-egamma.github.io.git
-git clone git@github.com:cms-egamma/cms-egamma.github.io.git
+
+git clone https://github.com/cms-egamma/cms-egamma.github.io.git
+#git@github.com:cms-egamma/cms-egamma.github.io.git
 #git config user.name github-actions
 #git config user.email github-actions@github.com
 cd cms-egamma.github.io/
